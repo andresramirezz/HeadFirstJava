d486109d86ff7025babca6df3a256feaf63b84fc
Andres Ramirez <andresramirezz@hotmail.com>
2020-04-24 10:15:53 -0400

    BeatBox MyReadInListener and MySendListener inner classes

--- src/com/headFirstJava/BeatBox/BeatBox.java
+++ src/com/headFirstJava/BeatBox/BeatBox.java
@@ -2,6 +2,7 @@ package com.headFirstJava.BeatBox;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 import java.util.ArrayList;
 import javax.sound.midi.*;
 import javax.swing.*;
@@ -182,5 +183,52 @@ public class BeatBox {
 			e.printStackTrace();
 		}
 		return event;
+	}
+	
+	public class MySendListener implements ActionListener{
+		public void actionPerformed( ActionEvent a ) {
+			boolean[] checkboxState = new boolean[256];
+			
+			for (int i = 0; i < 256; i++) {
+				JCheckBox check = (JCheckBox) checkboxList.get(i);
+				if ( check.isSelected()) {
+					checkboxState[i] = true;
+				}
+			}
+			
+			try {
+				FileOutputStream fileStream = new FileOutputStream( new File("Checkbox.ser"));
+				ObjectOutputStream os = new ObjectOutputStream(fileStream);
+				os.writeObject(checkboxState);
+			} catch ( Exception ex ) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	public class MyReadInListener implements ActionListener{
+		public void actionPerformed ( ActionEvent a ) {
+			boolean[] checkboxState = null;
+			try {
+				FileInputStream fileIn = new FileInputStream( new File ("Checkbox.ser"));
+				ObjectInputStream is = new ObjectInputStream(fileIn);
+				checkboxState = (boolean[]) is.readObject();
+			} 
+			catch ( Exception ex ) {
+				ex.printStackTrace();
+			}
+			for (int i = 0; i < 256; i++) {
+				JCheckBox check = (JCheckBox) checkboxList.get(i);
+				if ( checkboxState[i]) {
+					check.setSelected(true);
+				}
+				else {
+					check.setSelected(false);
+				}
+			}
+			
+			sequencer.stop();
+			buildTrackAndStart();
+		}
 	}
 }
