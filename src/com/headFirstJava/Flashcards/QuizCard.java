67ae0a225c8826d08de1593761accd5a200bc04e
Andres Ramirez <andresramirezz@hotmail.com>
2020-04-24 09:31:12 -0400

    Flashcards

---
+++ src/com/headFirstJava/Flashcards/QuizCard.java
@@ -0,0 +1,25 @@
+package com.headFirstJava.Flashcards;
+
+/**
+ * @author andresr
+ * File: QuizCard.java
+ * Contain the card data.
+ */
+public class QuizCard {
+	
+	private String question;
+	private String answer;
+	
+	public QuizCard(String q, String a) {
+		
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+	
+	public String getAnswer() {
+		return answer;
+	}
+
+}
